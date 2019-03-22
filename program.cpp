915e81a7cb5fbeffb3544567fd9733e74031e298
lauraroso <48685904+lauraroso@users.noreply.github.com>
2019-03-22 14:56:41 +0100

    imprimir en funcion separada

--- program.cpp
+++ program.cpp
@@ -1,28 +1,29 @@
+
 // Test File
 #include <iostream>                                     
 /** Multi linea
  * commentario
  */
- using namespace std;
- 
+
+using namespace std;
+void imprimir (int size, int coleccion[] )
+{
+    for(int i = 0; i < size; i++ )
+    {
+        cout << coleccion[i] << " " ;
+    }
+    cout<< endl;
+}
+
 int main(int cantidad, char* valores[])
-{     
-    int x [cantidad];
-   
+{   
+    int x [cantidad-1];
     cout << "Amount Parameters "<<cantidad<< endl;
  
-     
-     for(int i = 1; i < cantidad; i++ ){
-      
+     for(int i = 1; i < cantidad; i++ )
+    {
          x[i-1] = atoi(valores[i]);
-         cout << x[i-1] << endl ;
-
-    
-   }
-    
- 
-    
-    
-    
+}
+    imprimir(cantidad-1,x);
     return 0;                                         
 }
